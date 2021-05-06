d16e07727fa564092b2e36c6ab019264c4005269
Alvaro Ballon <alvarballon@gmail.com>
2021-05-06 12:05:26 -0400

    Added black and flake8

--- shop_list/ingredients.py
+++ shop_list/ingredients.py
@@ -1,14 +1,25 @@
+fr = {"Rice", "Chicken", "Bell Peppers", "Green Onions", "Eggs", "Garlic", "Ginger"}  # noqa
+bf = {"Tomatoes", "Cucumbers", "Fish", "Spinach", "Oil", "Lemons", "Cheese"}
+bc = {
+    "Onions",
+    "Curry",
+    "Ginger",
+    "Garlic",
+    "Milk",
+    "Butter",
+    "Chicken",
+    "Rice",
+    "Tomatoes",
+    "Chilli",
+    "Sugar",
+}
+ls = {"Onions", "Garlic", "Lentils", "Chicken Stock", "Steaks"}
+nd = {"Tomatoes", "Noodles", "Cheese", "Basil", "Sugar"}
+ch = {"Steak", "Onions", "Tomatoes", "Chilli", "Garlic", "Curry", "Beans", "Sugar"}  # noqa
+
+dic = {"fr": fr, "bf": bf, "bc": bc, "ls": ls, "nd": nd, "ch": ch}
 
 
-fr={"Rice", "Chicken", "Bell Peppers", "Green Onions", "Eggs", "Garlic", "Ginger"}
-bf={"Tomatoes", "Cucumbers", "Fish", "Spinach", "Oil", "Lemons", "Cheese"}
-bc={"Onions", "Curry", "Ginger","Garlic", "Milk", "Butter", "Chicken", "Rice", "Tomatoes", "Chilli", "Sugar"}
-ls={"Onions", "Garlic", "Lentils", "Chicken Stock", "Steaks"}
-nd={"Tomatoes", "Noodles", "Cheese", "Basil", "Sugar"}
-ch={"Steak", "Onions", "Tomatoes", "Chilli", "Garlic", "Curry", "Beans", "Sugar"}
-
-dic={"fr":fr,"bf":bf,"bc":bc, "ls":ls, "nd":nd, "ch":ch}
-
-def grocery_list(dish,ingredients):
-    missing = dic[dish]-ingredients
+def grocery_list(dish, ingredients):
+    missing = dic[dish] - ingredients
     return missing
