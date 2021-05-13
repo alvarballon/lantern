aa67f604d655a062a33cc9a15a0d62aa80644510
Alvaro Ballon <alvarballon@gmail.com>
2021-05-13 14:15:56 -0400

    deleted more bad files

--- shop_list.py
+++
@@ -1,8 +0,0 @@
-
-
-fr={"Rice", "Chicken", "Bell Peppers", "Green Onions", "Eggs", "Garlic", "Ginger"}
-bf={"Tomatoes", "Cucumbers", "Fish", "Spinach", "Oil", "Lemons", "Cheese"}
-bc={"Onions", "Curry", "Ginger","Garlic", "Milk", "Butter", "Chicken", "Rice", "Tomatoes", "Chilli", "Sugar"}
-ls={"Onions", "Garlic", "Lentils", "Chicken Stock", "Steaks"}
-nd={"Tomatoes", "Noodles", "Cheese", "Basil", "Sugar"}
-ch={"Steak", "Onions", "Tomatoes", "Chilli", "Garlic", "Curry", "Beans", "Sugar"}
